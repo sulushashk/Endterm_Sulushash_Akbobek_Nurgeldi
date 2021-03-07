8ef4d4fb337156173e343249d579a14934c2bf92
Akbobek-S <akbobek.munalbaeva@mail.ru>
2021-03-07 16:47:53 +0600

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,7 @@ package com.company;
 import com.company.controllers.*;
 import com.company.data.PostgresDB;
 import com.company.data.interfaces.IDB;
-import com.company.entities.BookInfo;
-import com.company.repositories.BookInfoRepo;
-import com.company.repositories.LibrarianRepo;
-import com.company.repositories.LibraryRepo;
-import com.company.repositories.ReaderRepo;
+import com.company.repositories.*;
 import com.company.repositories.interfaces.*;
 
 public class Main {
@@ -15,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         IDB db = new PostgresDB();
         IBookInfoRepo repo1 = new BookInfoRepo(db) {
-            IBookRepo repo2 = new BookRepo(db);
+            IBookRepo repo2 = new BookRepositories(db);
             ILibraryRepo repo3 = new LibraryRepo(db);
             ILibrarianRepo repo4 = new LibrarianRepo(db);
             IReaderRepo repo5 = new ReaderRepo(db);
@@ -24,8 +20,8 @@ public class Main {
             LibraryContr contr3 = new LibraryContr(repo3);
             LibrarianContr contr4 = new LibrarianContr(repo4);
             ReaderContr contr5 = new ReaderContr(repo5);
-            MyApplication app = new MyApplication(contr1.contr2, contr3, contr4, contr5);
-            app.start;
+            MyApplication app = new MyApplication(contr1, contr2, contr3, contr4, contr5);
+            app.start();
         }
     }
 }
